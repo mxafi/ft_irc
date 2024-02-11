eeb56ee1d0231492255d48e6d931435af31b5e95
Markus Laaksonen <markus@mxa.fi>
2024-02-11 08:50:34 +0200

    open listen socket

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,8 +1,51 @@
 #include "Server.h"
 
-irc::Server::Server(int port, std::string password)
+irc::Server::~Server() {
+  close(server_socket_fd_);
+  freeaddrinfo(srvinfo_);
+}
+
+irc::Server::Server(char* port, std::string password)
     : port_(port), password_(password) {}
 
-void irc::Server::start() {}
+int irc::Server::start() {
+  server_socket_fd_ = socket(server_socket_domain_, server_socket_type_,
+                             server_socket_protocol_);
+  if (server_socket_fd_ == SOCKET_FAILURE) {
+    PRINT_ERROR("server socket creation failed");
+    return FAILURE;
+  }
+
+  int fcntl_flags = fcntl(server_socket_fd_, F_GETFL);
+  if (fcntl_flags == FCNTL_FAILURE) {
+    PRINT_ERROR("server socket fcntl get flags failed");
+    return FAILURE;
+  }
+  if (fcntl(server_socket_fd_, F_SETFL, O_NONBLOCK) == FCNTL_FAILURE) {
+    PRINT_ERROR("server socket fcntl set nonblock failed");
+    return FAILURE;
+  }
+
+  hints_.ai_family = server_socket_domain_;
+  hints_.ai_socktype = server_socket_type_;
+  hints_.ai_flags = AI_PASSIVE;
+  if (getaddrinfo(NULL, port_, &hints_, &srvinfo_) != SUCCESS) {
+    PRINT_ERROR("server getaddrinfo failed");
+    return FAILURE;
+  }
+
+  if (bind(server_socket_fd_, srvinfo_->ai_addr, srvinfo_->ai_addrlen) ==
+      BIND_FAILURE) {
+    PRINT_ERROR("server bind failed");
+    return FAILURE;
+  }
+
+  if (listen(server_socket_fd_, SOMAXCONN) == LISTEN_FAILURE) {
+    PRINT_ERROR("server listen failed");
+    return FAILURE;
+  }
+
+  return SUCCESS;
+}
 
 void irc::Server::loop() {}
--- src/server/Server.h
+++ src/server/Server.h
@@ -1,20 +1,33 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <fcntl.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <string>
+
+#include "../common/error.h"
+#include "../common/magicNumber.h"
 
 namespace irc {
 
 class Server {
  private:
-  int server_socket_fd_;
-  int port_;
+  char* port_;
   std::string password_;
+  int server_socket_fd_;
+  int server_socket_domain_ = AF_INET;
+  int server_socket_type_ = SOCK_STREAM;
+  int server_socket_protocol_ = DEFAULT_SOCKET_PROTOCOL;
+  struct addrinfo hints_;
+  struct addrinfo* srvinfo_;
 
  public:
-  ~Server(){};
-  Server(int port, std::string password);
-  void start();
+  ~Server();
+  Server(char* port, std::string password);
+  int start();
   void loop();
 };
 
