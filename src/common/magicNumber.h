1659867f3ae23f5fe9d89f464909b158013cce81
Markus Laaksonen <markus@mxa.fi>
2024-02-11 08:47:59 +0200

    define important numbers related to sockets

--- src/common/magicNumber.h
+++ src/common/magicNumber.h
@@ -1,7 +1,14 @@
 #ifndef MAGICNUMBER_H
 #define MAGICNUMBER_H
 
+#define SUCCESS 0
+#define FAILURE 1
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+#define SOCKET_FAILURE -1
+#define FCNTL_FAILURE -1
+#define BIND_FAILURE -1
+#define LISTEN_FAILURE -1
+#define DEFAULT_SOCKET_PROTOCOL 0
 
 #endif
