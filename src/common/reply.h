efe4d3b29038c6470b1d2d6f19ac79c1af4d33e5
Markus Laaksonen <markus@mxa.fi>
2024-02-18 14:45:52 +0200

    add numeric replies 001-004

--- src/common/reply.h
+++ src/common/reply.h
@@ -1,9 +1,13 @@
 #ifndef REPLY_H
 #define REPLY_H
 
-// Numeric replies in order
-#define RPL_WELCOME_001()
+// Meta definitions
+#define RPL_META_MESSAGE(servername, numeric, message) (std::string(":") + servername + " " + numeric + " " + message + "\r\n")
 
-// Other replies
+// Numeric replies in order
+#define RPL_WELCOME_001(servername, nick, user, host) (RPL_META_MESSAGE(servername, "001", "Welcome to the Internet Relay Network " + nick + "!" + user "@" + host))
+#define RPL_YOURHOST_002(servername, version) (RPL_META_MESSAGE(servername, "002", "Your host is " + servername + ", running version " + version))
+#define RPL_CREATED_003(servername, date) (RPL_META_MESSAGE(servername, "003", "This server was created " + date))
+#define RPL_MYINFO_004(servername, version, user_modes, channel_modes) (RPL_META_MESSAGE(servername, "004", servername + " " + version + " " + user_modes + " " + channel_modes))
 
 #endif
