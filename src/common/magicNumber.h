37f9637e86c31ce6d4cf2853b3ea66afd788b713
Lionel Clerc <lionel.clerc@protonmail.com>
2024-02-20 15:57:25 +0200

    forgot to include

--- src/common/magicNumber.h
+++ src/common/magicNumber.h
@@ -11,5 +11,13 @@
 #define LISTEN_FAILURE -1
 #define DEFAULT_SOCKET_PROTOCOL 0
 #define POLL_FAILURE -1
+#define MESSAGE_MAX_AMOUNT_PARAMETERS 15
+#define MAX_MSG_LENGTH 512
+
+// Numeric reply names and numbers
+#define ERR_INPUTTOOLONG 417
+// According to RFC2812 Error codes > 502 are free, and on IRCv3 > 525 and < 670
+#define ERR_CUSTOM_ILLEGALNUL 530
+#define ERR_CUSTOM_TOOMANYPARAMS 531
 
 #endif
