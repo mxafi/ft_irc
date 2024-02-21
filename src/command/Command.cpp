0f2121b93ea8b11f8b019652a4b3b17e27ebdbe1
Diego James <die.james.talamas@gmail.com>
2024-02-21 14:46:42 +0200

    skeleton

--- src/command/Command.cpp
+++ src/command/Command.cpp
@@ -6,7 +6,67 @@
 /*   By: djames <djames@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:42:51 by djames            #+#    #+#             */
-/*   Updated: 2024/02/21 10:42:53 by djames           ###   ########.fr       */
+/*   Updated: 2024/02/21 14:17:27 by djames           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Command.h"
+
+extern std::string serverHostname_g;
+
+namespace irc {
+
+std::map<std::string, std::function<void(Command*)>> Command::commands = {
+    {"PING", [](Command* cmd) {
+       cmd->actionPing();
+     }},
+    {"Channel", [](Command* cmd) {
+       cmd->actionChannel();
+     }}
+};
+
+Command::Command(const std::string& commandString, Client& client)
+    : client_(client) {
+  parseCommand(commandString);
+}
+
+Command::~Command() {}
+
+void Command::execute() {
+  std::map<std::string, std::function<void(Command*)>>::iterator it =
+      commands.find(commandName_);
+  if (it != commands.end()) {
+
+    it->second(this);
+  } else {
+    LOG_DEBUG("not found command : " << commandName_);
+  }
+}
+
+void Command::parseCommand(
+    const std::string&
+        commandString) {  // tthis parssing is whatever but maybe we can talk with the messague waht twill come here
+  std::size_t pos = commandString.find(' ');
+  if (pos != std::string::npos) {
+    commandName_ = commandString.substr(0, pos);
+  } else {
+    commandName_ = commandString;
+  }
+}
+
+void Command::actionPing() {
+  std::string response = ":" + serverHostname_g + " PONG " + serverHostname_g +
+                         " :" + client_.getNickname();
+  LOG_DEBUG(response);
+}
+void Command::actionChannel() {
+  std::string response = ":" + serverHostname_g + " #newchannel " + serverHostname_g +
+                         " :" + client_.getNickname();
+  LOG_DEBUG(response);
+}
+
+// I was thinking here we can put it in a map and acording to the comand we create the response for example ping and pong
+// std::map<std::string, std::function<void()>> commands = {
+//     {"PING", &Command::actionPing}};
+
+}  // namespace irc
--- src/command/Command.h
+++ src/command/Command.h
@@ -6,7 +6,39 @@
 /*   By: djames <djames@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:42:29 by djames            #+#    #+#             */
-/*   Updated: 2024/02/21 10:42:30 by djames           ###   ########.fr       */
+/*   Updated: 2024/02/21 14:12:57 by djames           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include "../client/Client.h"
+#include "../common/log.h"
+#include "../common/magicNumber.h"
+
+#define MAX_BUFFER_SIZE 1024  //maybe this we change
+
+namespace irc {
+
+class Command {
+ public:
+  Command(const std::string& commandString, Client& client);
+  void execute();
+  void actionPing();
+  void actionChannel();
+  ~Command();
+
+ private:
+  std::string commandName_;
+  static std::map<std::string, std::function<void(Command*)>> commands;
+  void parseCommand(const std::string& commandString);
+  Client client_;
+};
+}  // namespace irc
+
+#endif
