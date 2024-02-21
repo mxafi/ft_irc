7ad2aca54e0c375f754380d31d9760228484b6cd
liocle <43987228+liocle@users.noreply.github.com>
2024-02-21 18:49:59 +0200

    Feature: Message class (#19)
    * First draft message class in namepace irc
    
    * uppercase Message class
    
    * Redefined Message class to suit RFC2812
    
    * lunch break
    
    * message class in progress
    
    * Message class
    
    * forgot to include
    
    * move test to proper directory
    
    * Message class wip
    
    * Message class without serialize function

--- src/common/magicNumber.h
+++ src/common/magicNumber.h
@@ -14,6 +14,16 @@
 #define DEFAULT_SOCKET_PROTOCOL 0
 #define TCP_PROTOCOL 6
 #define POLL_FAILURE -1
+#define MESSAGE_MAX_AMOUNT_PARAMETERS 15
+#define MAX_MSG_LENGTH 512
 #define NICK_MAX_LENGTH_RFC2812 9
+
+// Numeric reply names and numbers
+#define ERR_INPUTTOOLONG 417
+
+// Custom numeric replies
+// According to RFC2812 Error codes > 502 are free, and on IRCv3 > 525 and < 670
+#define ERR_CUSTOM_ILLEGALNUL 530
+#define ERR_CUSTOM_TOOMANYPARAMS 531
 
 #endif
---
+++ src/message/message.h
@@ -0,0 +1,40 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include "../common/log.h"
+#include "../common/magicNumber.h"
+
+namespace irc {
+
+class Message {
+ private:
+  std::string prefix_;
+  std::string command_;
+  std::vector<std::string> parameters_;
+  int numeric_;
+  void deserialize_(const std::string& serializedMessage);
+  void setPrefix_(std::istringstream& serializedStream);
+  void setCommand_(std::istringstream& serializedMessage);
+  void setParameters_(std::istringstream& serializedStream);
+
+  void checkNulChar(const std::string& serializedMessage);
+  void checkMessageLength(const std::string& serializedMessage);
+
+ public:
+  Message(std::string serializedMessage);
+
+  ~Message();
+
+  std::string getPrefix() const;
+  std::string getCommand() const;
+  int getNumeric() const;
+  std::vector<std::string> getParameters() const;
+
+  // std::string serialize() const;
+};
+
+}  // namespace irc
+#endif
