34c1b033eed738b2e99c9172fa35e5c631762df2
Markus Laaksonen <markus.laaksonen@duodecim.fi>
2024-03-06 20:22:58 +0200

    add long parameter to rpl 1-3

--- src/common/reply.h
+++ src/common/reply.h
@@ -7,9 +7,9 @@
 #define COM_MESSAGE(nick, user, host, command, params) (std::string(":") + nick + "!" + user + "@" + host + " " + command + " " + params + "\r\n")
 
 // Numeric replies in order
-#define RPL_WELCOME_001(servername, nick, user, host) (RPL_META_MESSAGE(servername, "001", "Welcome to the Internet Relay Network " + nick + "!" + user + "@" + host))
-#define RPL_YOURHOST_002(servername, version) (RPL_META_MESSAGE(servername, "002", "Your host is " + servername + ", running version " + version))
-#define RPL_CREATED_003(servername, date) (RPL_META_MESSAGE(servername, "003", "This server was created " + date))
+#define RPL_WELCOME_001(servername, nick, user, host) (RPL_META_MESSAGE(servername, "001", ":Welcome to the Internet Relay Network " + nick + "!" + user + "@" + host))
+#define RPL_YOURHOST_002(servername, version) (RPL_META_MESSAGE(servername, "002", ":Your host is " + servername + ", running version " + version))
+#define RPL_CREATED_003(servername, date) (RPL_META_MESSAGE(servername, "003", ":This server was created " + date))
 #define RPL_MYINFO_004(servername, version, user_modes, channel_modes) (RPL_META_MESSAGE(servername, "004", servername + " " + version + " " + user_modes + " " + channel_modes))
 
 #define RPL_ERR_NOSUCHCHANNEL_403(servername, channel) (RPL_META_MESSAGE(servername, "403", channel + " :No such channel"))
